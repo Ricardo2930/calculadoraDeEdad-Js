832d67049e3f5a012e5e0fd242f78f1626fa085a
Ricardo2930 <rhuaytan@gmail.com>
2023-05-16 16:04:21 -0500

    se actualizo el evento del boton

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
 const hoy = new Date();
 const diaHoy = hoy.getDate();
 //console.log(diaHoy);
-const mesHoy = hoy.getMonth()+1;
+const mesHoy = hoy.getMonth() + 1;
 // console.log(mesHoy);
 const añoHoy = hoy.getFullYear();
 // console.log(añoHoy)
 
+const año = document.getElementById("valorAño");
+const mes = document.getElementById("valorMes");
+const dia = document.getElementById("valorDia");
+const btn = document.querySelector("button");
 
-const año = document.getElementById("valorAño")
-const mes = document.getElementById("valorMes")
-const dia = document.getElementById("valorDia")
-
-
-const calcular = () => {
+btn.addEventListener("click", () => {
     const añoNacimiento = document.getElementById("año").value;
     const mesNacimiento = document.getElementById("mes").value;
     const diaNacimiento = document.getElementById("día").value;
-    
-    const edadAños = añoHoy - añoNacimiento
-    const edadMeses = mesHoy - mesNacimiento
-    const edadDias = diaHoy - diaNacimiento
 
-    if(mesHoy < mesNacimiento){
+    const edadAños = añoHoy - añoNacimiento;
+    const edadMeses = mesHoy - mesNacimiento;
+    const edadDias = diaHoy - diaNacimiento;
+
+    if (mesHoy < mesNacimiento) {
         let edadAñosMesMenor = edadAños - 1;
         //console.log(edadAñosMesMenor);
         año.innerHTML = edadAñosMesMenor;
 
         let edadMesesMesMenor = edadMeses + 11;
         //console.log(edadMesesMesMenor);
-        mes.innerHTML = edadMesesMesMenor; 
+        mes.innerHTML = edadMesesMesMenor;
 
         let edadDiasMesMenor = edadDias + 30;
         //console.log(edadDiasMesMenor);
         dia.innerHTML = edadDiasMesMenor;
 
-            if(diaHoy == diaNacimiento || diaHoy > diaNacimiento){
-                let m=edadMesesMesMenor + 1;
-                //console.log(m);
-                mes.innerHTML=m;
-                let a = edadDias
-               //console.log(a);
-                dia.innerHTML=a;
-            }
-    }
-
-    else if(mesHoy == mesNacimiento && diaHoy < diaNacimiento) {
+        if (diaHoy == diaNacimiento || diaHoy > diaNacimiento) {
+            let m = edadMesesMesMenor + 1;
+            //console.log(m);
+            mes.innerHTML = m;
+            let a = edadDias;
+            //console.log(a);
+            dia.innerHTML = a;
+        }
+    } else if (mesHoy == mesNacimiento && diaHoy < diaNacimiento) {
         let edadAñosMesIgualDiaMenor = edadAños - 1;
         //console.log(edadAñosMesIgualDiaMenor)
         año.innerHTML = edadAñosMesIgualDiaMenor;
@@ -56,32 +53,23 @@ const calcular = () => {
         let edadDiasMesIgualDiaMenor = edadDias + 30;
         //console.log(edadDiasMesIgualDiaMenor);
         dia.innerHTML = edadDiasMesIgualDiaMenor;
-    }
-
-    else if (diaHoy == diaNacimiento && mesHoy > mesNacimiento){
-        año.innerHTML = edadAños
+    } else if (diaHoy == diaNacimiento && mesHoy > mesNacimiento) {
+        año.innerHTML = edadAños;
         let edadMesesDiaIgualMesMayor = edadMeses;
         //console.log(edadMesesDiaIgualMesMayor);
         mes.innerHTML = edadMesesDiaIgualMesMayor;
-        dia.innerHTML=edadDias;
-    }
-
-    else if (diaHoy < diaNacimiento && mesHoy > mesNacimiento){
+        dia.innerHTML = edadDias;
+    } else if (diaHoy < diaNacimiento && mesHoy > mesNacimiento) {
         año.innerHTML = edadAños;
-        let m=edadMeses-1;
+        let m = edadMeses - 1;
         //console.log(m);
-        mes.innerHTML=m;
-        let a = edadDias + 30
+        mes.innerHTML = m;
+        let a = edadDias + 30;
         //console.log(a);
-        dia.innerHTML=a;
+        dia.innerHTML = a;
+    } else {
+        año.innerHTML = edadAños;
+        mes.innerHTML = edadMeses;
+        dia.innerHTML = edadDias;
     }
-
-    else {
-        año.innerHTML=edadAños;
-        mes.innerHTML=edadMeses;
-        dia.innerHTML=edadDias;
-    }  
-}
-
-
-
+});

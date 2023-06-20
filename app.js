e2d80d2819b381366c8b9ad40a979674f7692f62
Ricardo2930 <rhuaytan@gmail.com>
2023-06-20 11:24:57 -0500

    update app

--- app.js
+++ app.js
@@ -1,120 +1,126 @@
-//Obtenemos la fecha actual
-const hoy = new Date(); //Obtenemos la fecha de Hoy con new Date()
-const diaHoy = hoy.getDate(); //Obtenemos el dia de la fecha de Hoy con getDate()
-const mesHoy = hoy.getMonth() + 1; //Obtenemos el mes de la fecha de Hoy con getMonth()
-const añoHoy = hoy.getFullYear(); //Obtenemos el año de la fecha de Hoy con getFullYear()
+const hoy = new Date();
+const diaHoy = hoy.getDate();
+const mesHoy = hoy.getMonth() + 1;
+const añoHoy = hoy.getFullYear(); 
 
-//Capturamos los elementos del DOM a utiilizar , del div del HTML con class = "resultados" a traves del id por medio del document.getElementById()
+const calcularEdad = document.querySelector("button");
+const inputs = document.querySelectorAll(".inputs");
+const inputAñoNacimiento = document.getElementById("año");
+const inputMesNacimiento = document.getElementById("mes");
+const inputDiaNacimiento = document.getElementById("día");
+
 const año = document.getElementById("valorAño");
 const mes = document.getElementById("valorMes");
 const dia = document.getElementById("valorDia");
-const btn = document.querySelector("button"); //Capturamos el button de HTML a traves del document.querySelector()
 
-//Capturamos los elementos(inputs) del DOM en el formulario, del div del HTML con class="data" a traves del id por medio del document.getElementById()
-const añoNac = document.getElementById("año");
-const mesNac = document.getElementById("mes");
-const diaNac = document.getElementById("día");
-
-//Validamos las entradas de cada input (dia, mes, año) de nacimiento al realizarse el evento blur.
-//Sino cumple con la condición a evaluar en diaNac, mesNac y AñoNac, se agrega una classe que contiene un texto adicional y un estilo en el input.
-diaNac.addEventListener("blur", (e) => {
-    const diaNacim = e.target.value; //Capturamos el valor del elemento por el parámetro
-    if (diaNacim > 31) {
-        document.getElementById("data-dia").classList.add("data-incorrecta");
-    }
-});
-
-mesNac.addEventListener("blur", (e) => {
-    const mesNacim = e.target.value;
-    if (mesNacim > 12) {
-        document.getElementById("data-mes").classList.add("data-incorrecta");
-    }
-});
-
-añoNac.addEventListener("blur", (e) => {
-    const añoNacim = e.target.value;
-    if (añoNacim > añoHoy || añoNacim < 1900) {
-        document.getElementById("data-año").classList.add("data-incorrecta");
-    }
-    if (añoNacim.length < 4) {
-        document.getElementById("data-año").classList.add("data-incorrecta");
-    }
-});
-
-//Iniciamos la funcion que realizara el Calculo de la Edad
-btn.addEventListener("click", () => {
+const calculate = () => {
     //Capturamos los valores ingresados en los inputs (dia, mes,año) de la Fecha de Nacimiento
-    const añoNacimiento = añoNac.value;
-    const mesNacimiento = mesNac.value;
-    const diaNacimiento = diaNac.value;
-    //Calculamos la diferencia entre años, meses y dias actuales frente a los de nacimiento
-    const edadAños = añoHoy - añoNacimiento;
-    const edadMeses = mesHoy - mesNacimiento;
-    const edadDias = diaHoy - diaNacimiento;
+    const añoDeNacimiento = inputAñoNacimiento.value;
+    const mesDeNacimiento = inputMesNacimiento.value;
+    const diaDeNacimiento = inputDiaNacimiento.value;
 
-    //Validamos cuando el mes actual es menor al mes de nacimiento ingresado
-    if (mesHoy < mesNacimiento) {
-        let edadAñosMesMenor = edadAños - 1;
-        año.innerHTML = edadAñosMesMenor;
+    const fechaActual = new Date(hoy.getFullYear(),hoy.getMonth(),hoy.getDate());
+    const fechaDeNacimiento = new Date(añoDeNacimiento,mesDeNacimiento - 1,diaDeNacimiento);
+  
+    if (fechaDeNacimiento <= fechaActual) {
+        
+        //Calculamos la diferencia entre años, meses y dias actuales frente a los de nacimiento
+        let edadAños = añoHoy - añoDeNacimiento;
+        let edadMeses = mesHoy - mesDeNacimiento;
+        let edadDias = diaHoy - diaDeNacimiento;
 
-        let edadMesesMesMenor = edadMeses + 11;
-        mes.innerHTML = edadMesesMesMenor;
-
-        let edadDiasMesMenor = edadDias + 30;
-        dia.innerHTML = edadDiasMesMenor;
-        //Validamos cuando el mes actual es menor al mes de nacimiento ingresado y el dia actual es igual al dia de nacimiento o el dia actual es mayor al dia de nacimiento
-        if (diaHoy == diaNacimiento || diaHoy > diaNacimiento) {
-            let edadMesDiaHoyIgualoDiaHoyMayor = edadMesesMesMenor + 1;
-            mes.innerHTML = edadMesDiaHoyIgualoDiaHoyMayor;
-
-            let edadDiasDiaHoyIgualoDiaHoyMayor = edadDias;
-            dia.innerHTML = edadDiasDiaHoyIgualoDiaHoyMayor;
-        }
-    }
-    //Validamos cuando el mes actual es igual al mes de nacimiento y el dia actual es menor al dia de nacimiento
-    else if (mesHoy == mesNacimiento && diaHoy < diaNacimiento) {
-        let edadAñosMesIgualDiaMenor = edadAños - 1;
-        año.innerHTML = edadAñosMesIgualDiaMenor;
-
-        let edadMesesMesIgualDiaMenor = edadMeses + 11;
-        mes.innerHTML = edadMesesMesIgualDiaMenor;
-
-        let edadDiasMesIgualDiaMenor = edadDias + 30;
-        dia.innerHTML = edadDiasMesIgualDiaMenor;
-    }
-    //Validamos cuando el dia actual es igual al dia de nacimiento y el mes actual es mayor al mes de nacimiento
-    else if (diaHoy == diaNacimiento && mesHoy > mesNacimiento) {
-        año.innerHTML = edadAños;
-
-        let edadMesesDiaIgualMesMayor = edadMeses;
-        mes.innerHTML = edadMesesDiaIgualMesMayor;
-        dia.innerHTML = edadDias;
-    }
-    //Validamos cuando el dia actual es menor al dia de nacimiento y el mes actual es mayor al mes de nacimiento
-    else if (diaHoy < diaNacimiento && mesHoy > mesNacimiento) {
-        año.innerHTML = edadAños;
-        let edadMesesDiaHoyMenorYMesHoyMayor = edadMeses - 1;
-        mes.innerHTML = edadMesesDiaHoyMenorYMesHoyMayor;
-
-        let edadDiasDiaHoyMenorYMesHoyMayor = edadDias + 30;
-        dia.innerHTML = edadDiasDiaHoyMenorYMesHoyMayor;
-    } else {
         año.innerHTML = edadAños;
         mes.innerHTML = edadMeses;
         dia.innerHTML = edadDias;
-    }
-    //Si la longitud de algun input es 0, que no calcule la edad, por falta de datos
-    if (diaNacimiento.length === 0 || mesNacimiento.length === 0 || añoNacimiento.length === 0) {
-        dia.innerHTML = "--";
-        mes.innerHTML = "--";
-        año.innerHTML = "--";
-    }
 
-    if(diaNacimiento < 32 && mesNacimiento < 13){
+        if (edadMeses < 0 || (edadMeses == 0 && edadDias < 0)) {
+            año.innerHTML = edadAños - 1;
+            mes.innerHTML = edadMeses + 11;
+            dia.innerHTML = edadDias + 31;
 
-    }else{
-        dia.innerHTML = "--";
-        mes.innerHTML = "--";
+            if (edadDias >= 0) {
+                mes.innerHTML = edadMeses + 12;
+                dia.innerHTML = edadDias;
+            }
+        }
+
+        if (diaHoy < diaDeNacimiento && mesHoy > mesDeNacimiento && añoDeNacimiento <= añoHoy) {
+            mes.innerHTML = edadMeses - 1;
+            dia.innerHTML = edadDias + 31;
+        }
+
+        if(inputDiaNacimiento.value.length==0,inputMesNacimiento.value.length==0,inputAñoNacimiento.value.length==0){
+            año.innerHTML = "--";
+            mes.innerHTML = "--";
+            dia.innerHTML = "--";   
+        }
+    } else {
+        document.getElementById("data-dia").classList.add("data-incorrecta");
+        document.getElementById("data-mes").classList.add("data-incorrecta");
+        document.getElementById("data-año").classList.add("data-incorrecta")
         año.innerHTML = "--";
+        mes.innerHTML = "--";
+        dia.innerHTML = "--";
     }
+};
+
+const validarInputs = (e) => {
+    let valorInput = e.target.name;
+    switch (valorInput) {
+        case "dia":
+            if (!inputDiaNacimiento.value) {
+                document.getElementById("data-dia").classList.add("data-incorrecta");
+                calcularEdad.disabled = true;
+            } else if (inputDiaNacimiento.value > 31 || inputDiaNacimiento.value == 0) {
+                document.getElementById("data-dia").classList.add("data-incorrecta");
+                calcularEdad.disabled = true;
+            }
+            break;
+
+        case "mes":
+            if (!inputMesNacimiento.value) {
+                document.getElementById("data-mes").classList.add("data-incorrecta");
+                calcularEdad.disabled = true;
+            } else if (inputMesNacimiento.value > 12 || inputMesNacimiento.value == 0) {
+                document.getElementById("data-mes").classList.add("data-incorrecta");
+                calcularEdad.disabled = true;
+            }
+
+            if ((inputMesNacimiento.value == 4 || inputMesNacimiento.value == 6 || inputMesNacimiento.value == 9 || inputMesNacimiento.value == 11) && inputDiaNacimiento.value > 30) {
+                document.getElementById("data-mes").classList.add("data-incorrecta-nacim");
+                calcularEdad.disabled = true;
+            }
+
+            if ((inputMesNacimiento.value == 2) && (inputDiaNacimiento.value>29)){
+                document.getElementById("data-dia").classList.add("data-incorrecta-nacim");
+                document.getElementById("data-mes").classList.add("data-incorrecta-nacim");
+                calcularEdad.disabled = true;
+            }
+            break;
+
+        case "año":
+            if (!inputAñoNacimiento.value) {
+                document.getElementById("data-año").classList.add("data-incorrecta");
+                calcularEdad.disabled = true;
+            } else if (inputAñoNacimiento.value > añoHoy || inputAñoNacimiento.value < 1900 || inputAñoNacimiento.value.length !== 4) {
+                document.getElementById("data-año").classList.add("data-incorrecta");
+                calcularEdad.disabled = true;
+            }
+
+            if((inputDiaNacimiento.value == 29) && (inputMesNacimiento.value == 2) && !(inputAñoNacimiento.value % 400 === 0 && inputAñoNacimiento.value % 100 !== 0 || inputAñoNacimiento.value % 4 === 0)){
+                document.getElementById("data-año").classList.add("data-incorrecta-nacim")
+                calcularEdad.disabled = true;
+               }
+            break;
+
+        default:
+            calculate();
+            break;
+    }  
+};
+
+inputs.forEach((input) => {
+    input.addEventListener("blur", validarInputs);
 });
+
+calcularEdad.addEventListener("click", validarInputs);
